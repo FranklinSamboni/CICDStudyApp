133377fd34acde05640ff5681f282487b08b8f76
franklin.samboni <franklin.samboni@globant.com>
2022-08-05 17:14:33 -0500

    Initial Commit

---
+++ CICDStudyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDStudyApp
+//
+//  Created by Franklin Samboni Castillo on 5/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
